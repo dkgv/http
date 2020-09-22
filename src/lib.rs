3645992ccb7e879cd0024cdf92b6d778c8f3f1af
Gustav <gustav-git@pm.me>
2020-09-22 18:29:16 +0200

    Open socket at specified port

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,9 @@ mod response;
 mod route;
 
 use route::{HttpMethod, Route};
+use request::{Request};
+use std::net::{SocketAddr, TcpListener};
+use socket2::{Socket, Domain, Type};
 use std::collections::HashMap;
 
 type Handler = fn(request: request::Request) -> response::Response;
@@ -13,22 +16,22 @@ struct HttpServer {
 }
 
 impl HttpServer {
-    fn bind(port: i16) -> HttpServer {
+    pub fn bind(port: i16) -> HttpServer {
         HttpServer {
             port: port,
             routes: HashMap::new()
         }
     }
 
-    fn get(self, endpoint: &String, handler: Handler) -> HttpServer {
+    pub fn get(self, endpoint: &String, handler: Handler) -> HttpServer {
         self.route(endpoint, HttpMethod::Get, handler)
     }
 
-    fn post(self, endpoint: &String, handler: Handler) -> HttpServer {
+    pub fn post(self, endpoint: &String, handler: Handler) -> HttpServer {
         self.route(endpoint, HttpMethod::Post, handler)
     }
 
-    fn route(mut self, endpoint: &String, method: HttpMethod, handler: Handler) -> HttpServer {
+    pub fn route(mut self, endpoint: &String, method: HttpMethod, handler: Handler) -> HttpServer {
         let route = Route::new(endpoint, method);
         if self.routes.contains_key(&route) {
             panic!("Unable to define duplicate route {} with specified HTTP method.", endpoint);
@@ -37,7 +40,24 @@ impl HttpServer {
         self
     }
 
-    fn listen() {
-        // TODO socket
+    pub fn launch(self) {
+        let address = format!("127.0.0.1:{}", self.port);
+        println!("Launching at {}...", address);
+
+        let socket = Socket::new(Domain::ipv4(), Type::stream(), None).unwrap();
+        socket.bind(&address.parse::<SocketAddr>().unwrap().into()).unwrap();
+        self.listen(socket.into_tcp_listener());
+
+        println!("Done!");
+    }
+
+    async fn listen(self, listener: TcpListener) {
+        loop {
+            let result = listener.accept();
+        }
+    }
+
+    async fn handle_request(self, request: Request) {
+
     }
 }
