8bf4b62e313c58c7a7d97456d795aa21418f3e72
Gustav <gustav-git@pm.me>
2020-09-27 19:08:28 +0200

    Add example

---
+++ example/src/main.rs
@@ -0,0 +1,20 @@
+use toy_http::request::Request;
+use toy_http::response::Response;
+use toy_http::HttpServer;
+
+fn index(request: &Request) -> Response {
+    Response::ok("Hello world")
+}
+
+fn api_param(request: &Request) -> Response {
+    let param: i32 = request.get_param("param");
+    let body = format!("Received int {}", param);
+    Response::ok(&body)
+}
+
+fn main() {
+    let server = HttpServer::bind(5000)
+        .get("/", index)
+        .get("/api/<param:int>", api_param);
+    server.launch();
+}
